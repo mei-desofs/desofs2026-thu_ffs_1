a091a9bbb8dad0c63a1b879be165cd1c91584aac
beatrizoliveirasilva <1200347@isep.ipp.pt>
2026-05-14 22:14:26 +0100

    refactor AuthenticationService to use BCryptPasswordEncoder and improve password validation logic #9

--- cantinas-cinfaes-backend/src/main/java/bioCanteenApp/authentication/service/AuthenticationService.java
+++ cantinas-cinfaes-backend/src/main/java/bioCanteenApp/authentication/service/AuthenticationService.java
@@ -6,36 +6,38 @@ import bioCanteenApp.users.domain.User;
 import bioCanteenApp.users.mapper.UserMapper;
 import bioCanteenApp.users.repository.IUserRepo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationService implements IAuthenticationService {
 
-    private final PasswordEncoder passwordEncoder;
+    private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
     private final IUserRepo userRepo;
     private final UserMapper userMapper;
 
     @Override
     public LoginResponse login(LoginDTO dto) {
-
         User user = userRepo.findByEmail(dto.getEmail())
                 .orElse(null);
 
-        if (user == null) return null;
+        if (user == null) {
+            return null;
+        }
 
-        boolean validPassword = passwordEncoder.matches(
-                dto.getPassword(),
-                user.getPassword()
-        );
-
-        if (!validPassword) return null;
+        if (!passwordEncoder.matches(dto.getPassword(), decodePassword(user.getPassword()))) {
+            return null;
+        }
 
         return LoginResponse.builder()
                 .token("JWT Token")
                 .tokenType("Bearer")
                 .user(userMapper.toDTO(user))
                 .build();
+    }
+
+    private String decodePassword(String encodedPassword) {
+        return passwordEncoder.encode(encodedPassword);
     }
 }
