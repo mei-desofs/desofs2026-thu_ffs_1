ecac0fd07d6ee17fcf112e3a8b2de0c6b683e7c3
Carolina Sousa <1220618@isep.ipp.pt>
2026-06-13 12:44:42 +0100

    fix Menu Service

--- cantinas-cinfaes-backend/src/main/java/bioCanteenApp/menu/service/MenuService.java
+++ cantinas-cinfaes-backend/src/main/java/bioCanteenApp/menu/service/MenuService.java
@@ -16,6 +16,7 @@ import bioCanteenApp.products.repository.ProductBatchRepo;
 import bioCanteenApp.products.service.ProductService;
 import bioCanteenApp.users.domain.Role;
 import bioCanteenApp.users.domain.User;
+import bioCanteenApp.users.dto.GetUserDTO;
 import bioCanteenApp.users.dto.UserDTO;
 import bioCanteenApp.users.mapper.UserMapper;
 import bioCanteenApp.users.repository.UserRepo;
